470238fa794298621edc545b4587d4f6100bfc74
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-08-10 10:58:53 +0530

    changing port

--- main.py
+++ main.py
@@ -56,5 +56,5 @@ async def get_net_image_prediction(prompt: str = ""):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     run(app, host="0.0.0.0", port=port)
